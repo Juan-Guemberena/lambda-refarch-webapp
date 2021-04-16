0fba7f9217f3a1183d29c19b14027678f0e5ab87
Juan-Guemberena <chinoguembe@gmail.com>
2021-04-16 11:35:27 -0300

    test readonly ext id
    Signed-off-by: Juan-Guemberena <chinoguembe@gmail.com>

--- www/src/ToDo.js
+++ www/src/ToDo.js
@@ -19,7 +19,7 @@ function ToDo({ toDos, addAWSAccount, deleteToDo, completeToDo }) {
               <Label for="newRole" hidden>Role</Label>
               <Input type="text" name="role" id="newRole" placeholder="RoleARN" />
               <Label for="newExternalID" hidden>ExtID</Label>
-              <Input type="text" name="extID" id="externalID" placeholder="External-ID" readonly />
+              <Input type="text" name="extID" id="externalID" placeholder="External-ID" readonly="true"/>
             </FormGroup>
             <Button onClick={addAWSAccount} color="primary" className="ml-1">Connect</Button>
           </Form>
