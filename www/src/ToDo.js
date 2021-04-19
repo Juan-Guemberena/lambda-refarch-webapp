8f7407df6805123cf352109a1388f16f8a860d26
Juan-Guemberena <chinoguembe@gmail.com>
2021-04-19 15:35:26 -0300

    test cheat button v2
    Signed-off-by: Juan-Guemberena <chinoguembe@gmail.com>

--- www/src/ToDo.js
+++ www/src/ToDo.js
@@ -6,15 +6,17 @@ import './ToDo.css';
 
 function ToDo({ toDos, addAWSAccount, deleteToDo, completeToDo }) {
   
-
+  var isCheating = false
 
   function cheat_button(){
 
     if(document.getElementById("externalID").hasAttribute("readonly")){
       document.getElementById("externalID").removeAttribute("readonly");
+      isCheating = true;
     }
     else{
       document.getElementById("externalID").setAttribute("readonly","true");
+      isCheating = false
     }
   
   }
@@ -38,7 +40,7 @@ function ToDo({ toDos, addAWSAccount, deleteToDo, completeToDo }) {
               <Input type="text" name="extID" id="externalID" placeholder="example_insecure_external_id" readonly="true"/>
             </FormGroup>
             <Button onClick={addAWSAccount} color="primary" className="ml-1">Connect</Button>
-            <Button id="cheat_button" onClick={() => {cheat_button()} } color="primary" className="ml-1">Cheat</Button>
+            <Button id="cheat_button" onClick={() => {cheat_button()} } color="primary" className="ml-1">{isCheating ? "Uncheat" : "Cheat" }</Button>
           </Form>
         </Col>
         <Col xs="12" className="mt-1 mb-1">
