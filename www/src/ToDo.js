329dfb2a543b52195e74e3b0e8116ada171c3b40
Juan-Guemberena <chinoguembe@gmail.com>
2021-04-19 15:11:33 -0300

    test cheat button v2
    Signed-off-by: Juan-Guemberena <chinoguembe@gmail.com>

--- www/src/ToDo.js
+++ www/src/ToDo.js
@@ -1,25 +1,19 @@
 import React, { useState } from 'react';
-import { Button, ButtonGroup, ToggleButton, Form, FormGroup, Input, Label, Row, Col } from 'reactstrap';
+import { Button, ButtonGroup, Form, FormGroup, Input, Label, Row, Col } from 'reactstrap';
 import './ToDo.css';
 
 
 
 function ToDo({ toDos, addAWSAccount, deleteToDo, completeToDo }) {
   
-  function removeReadonly() {
-    document.getElementById("externalID").removeAttribute("readonly");
-  }
+
 
   function cheat_button(){
 
-    document.getElementById("externalID").removeAttribute("readonly");
+    console.log(document.getElementById("externalID").removeAttribute("readonly"));
 
-    if (!document.getElementById("externalID").removeAttribute("readonly")){
-      document.getElementById("externalID").setAttribute("readonly","true");
-    }
-    else{
-      document.getElementById("externalID").removeAttribute("readonly");
-    }
+    document.getElementById("externalID").removeAttribute("readonly");
+    
   
   }
   
@@ -42,7 +36,7 @@ function ToDo({ toDos, addAWSAccount, deleteToDo, completeToDo }) {
               <Input type="text" name="extID" id="externalID" placeholder="example_insecure_external_id" readonly="true"/>
             </FormGroup>
             <Button onClick={addAWSAccount} color="primary" className="ml-1">Connect</Button>
-            <ToggleButton id="cheat_button" onClick={() => {cheat_button()} } color="primary" className="ml-1">Cheat</ToggleButton>
+            <Button id="cheat_button" onClick={() => {cheat_button()} } color="primary" className="ml-1">Cheat</Button>
           </Form>
         </Col>
         <Col xs="12" className="mt-1 mb-1">
