65e84782d3054a5e65f42c29e5ffa0187fea7758
Juan-Guemberena <chinoguembe@gmail.com>
2021-04-28 13:54:56 -0300

    Fix commit
    Signed-off-by: Juan-Guemberena <chinoguembe@gmail.com>

--- www/src/ExternalAttack.js
+++ www/src/ExternalAttack.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Button, Form, FormGroup, Input, Label, Row, Col } from 'reactstrap';
 import './Attack.css';
-import './InternalAttack';
+import InternalAttack from './InternalAttack';
 
 
 function ExternalAttack({  addAWSAccount }) {
