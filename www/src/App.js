b2c1a1da4ba5e3a3695fb8e61f3bf902a90fb20c
Juan-Guemberena <chinoguembe@gmail.com>
2021-07-05 21:01:47 -0300

    Changed app and internal attack to react components, missing external attack
    Signed-off-by: Juan-Guemberena <chinoguembe@gmail.com>

--- www/src/App.js
+++ www/src/App.js
@@ -1,15 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import { Container, Jumbotron, Row, Col, Button } from 'reactstrap';
 import './App.css';
+import InternalAttack from './InternalAttack';
 import logo from './aws.png';
 import config from './config';
 
-function App() {
-  const [idToken, setIdToken] = useState('');
-  const [role, setRole] = useState('');
-  const [extID,setExtID] = useState('');
-  const [successful,setSuccess] = useState(false);
-  const [isInternal,setInternal] = useState(true);
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      idToken: '',
+      role: '',
+      extID: '',
+      successful: false,
+      attack: 'internal'
+    };
+  };
+
+  setIdToken(idToken){
+    this.setState({
+      idToken: idToken
+    })
+  }
 
   useEffect(() => {
     getIdToken();
@@ -17,25 +30,22 @@ function App() {
 
 
 
-  const clearCredentials = () => {
-    window.location.href = config.redirect_url;
-  }
 
-  const getIdToken = () => {
+
+  function getIdToken(){
     const hash = window.location.hash.substr(1);
     const objects = hash.split("&");
     objects.forEach(object => {
       const keyVal = object.split("=");
       if (keyVal[0] === "id_token") {
-        setIdToken(keyVal[1]);
+        this.setIdTokensetIdToken(keyVal[1]);
       }
     });
   };
 
 
-
-  return (
-    <div className="App">
+  render(){
+    return <div className="App">
       <Container>
         <Jumbotron>
           <Row>
@@ -50,8 +60,7 @@ function App() {
               {idToken.length > 0 ?
                 (
                 <Row>
-                  {isInternal ? (<InternalAttack/>): (<ExternalAttack/>)
-}
+                  {isInternal ? (<InternalAttack props={this.state} />): (<ExternalAttack/>)}
                   </Row>
                 ) : (
                   <Row><Button
@@ -74,87 +83,10 @@ function App() {
         </Jumbotron>
       </Container>
     </div>
-  );
 }
 
 
-function InternalAttack() {
 
-  const [showHover,setHover] = useState(false);
-
-  
-  
-  return (
-    <div className="InternalAttack" >
-      <Row>
-        <Col xs="12" className="mt-1 mb-1">
-            <h3>Internal demo attack</h3>
-            <p>This attack can happen when a user inside an AWS account has insecure credentials, and they get stolen by a malicious 3rd party.
-               Inside the SaaS vendor's cloud, you'll find that sometimes there are "sts:assumeRole":"*" type of permissions, only limited by the external-id inside the clients cloud.
-               The thing is, you don't need the external-id if you want to attack yourself and the cloud isn't well configured, so the only thing you need to find is a high privilege role to assume.
-            </p>
-          <Form>
-            <FormGroup>
-              <Label for="newRole" hidden>Role</Label>
-              <Input type="text" name="role" id="newRole" placeholder="role-name"/>
-            </FormGroup>
-            <Button onClick={internalAttack} color="primary" className="ml-1">Connect</Button>
-            <Button id="brute_force_button" onClick={() => {brute_force_button()} } onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)} color="primary" className="ml-1">Brute Force</Button>
-            {showHover && (<div className="hoverText" id="brute_force_text">Click to simulate running a tool like Pacu to brute force the role (by trying known or common words and numbers and their combinations)</div>)}
-          </Form>
-        </Col>      
-      </Row>
-      <br></br>
-      <Row>
-      <Button onclick={() => {<ExternalAttack/>}} color="primary" className="ml-1">Advanced Attack</Button>
-      </Row>
-    </div >
-  );
-}
-
-function brute_force_button(){
-  var role_txt = document.getElementById('newRole');
-  var roles = ["SSC", "SSC-role", "SecureCloud-role", "SecureCloudRole", "SSC_role_Dev", "SSC_role_prod", "SSC_role"];
-  role_txt.value = roles[0];
-  for(let i = 1 ; i < roles.length ; i++){
-      setTimeout(function() {
-          role_txt.value = roles[i];
-      }, i*1000);
-  }      
-}
-
-
-const internalAttack = async (event) => {
-const newRoleInput = document.getElementById("newRole");
-setRole(newRoleInput.value);
-
-if (!role || role === ''){
-  document.getElementById("hidden_jumbotron").setAttribute("hidden","true");
-  return;
-}
-
-
-const newAccount = {
-  "roleARN": role,
-};
-
-axios.defaults.headers.post['Authorization'] = idToken
-
-const result = await axios({
-  method: 'POST',
-  url: `${config.api_base_url}`,
-  data: newAccount
-});
-
-
-if (result && result.status === 401) {
-  clearCredentials();
-} else if (result && result.status === 200) {
-  newRoleInput.value = '';
-  if (result.data.message === 'Connection Successful') {setSuccess(true);} else {setSuccess(false)}
-  document.getElementById("hidden_jumbotron").removeAttribute("hidden");
-}
-}
 
 function ExternalAttack() {
 
