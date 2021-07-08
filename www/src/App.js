685763e108ef475ea494eef3908a8f294ffc8e0d
Juan-Guemberena <chinoguembe@gmail.com>
2021-07-08 11:25:48 -0300

    Added export
    Signed-off-by: Juan-Guemberena <chinoguembe@gmail.com>

--- www/src/App.js
+++ www/src/App.js
@@ -6,7 +6,7 @@ import logo from './aws.png';
 import config from './config';
 
 
-class App extends React.Component {
+export default class App extends React.Component {
   constructor(){
     super();
     this.state = {
